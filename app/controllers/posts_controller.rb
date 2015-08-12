340ae5a012e03e0003a60aba2dcf865c62fdcd47
Denisse Lara <dmlaramartin@gmail.com>
2015-08-12 12:37:22 -0400

    Show all tags and their frequencies

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @tags_with_frequency = Tag.all.map do |tag| 
+      [tag, tag.posts.length]
+    end
   end
 
   def show
