9d4063893c1ad01d2f254c2bd27639f81e403274
Denisse Lara <dmlaramartin@gmail.com>
2015-08-13 16:11:17 -0400

    Show author's name in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, :description, presence: true
   validates :title, length: { maximum: 100 }
 
-  belongs_to :post
+  belongs_to :author
   has_and_belongs_to_many :tags
 end
