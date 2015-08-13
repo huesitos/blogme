e5de62b1a3192f77c613598e1d1213d23402e62d
Denisse Lara <dmlaramartin@gmail.com>
2015-08-13 12:23:25 -0400

    Modify rails generator
    - Remove automatic generation of helpers, css, and js files

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module Blogme
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.helper false
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
