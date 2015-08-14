45b93b1fb26b20ad3c7bc1dec7490081604af82f
Denisse Lara <dmlaramartin@gmail.com>
2015-08-14 09:50:38 -0400

    Session login template

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     name "Denisse Margarita"
     last_name "Lara Martin"
     email "dmlaramartin@gmail.com"
-    password Faker::Internet.password(6)
+    password '123456'
     image "https://lh5.googleusercontent.com/-WmhCxsI_pds/VW--rLHXvrI/AAAAAAAAAcM/UvKhmhf7XCI/s778-no/IMG_0253.JPG"
     message "There is nothing quite so useless, as doing with great efficiency, something that should not be done at all. - Peter Drucker"
   end
