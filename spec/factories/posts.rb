1c844c42c955a0190c21bdf451348d4a068ed6bb
Denisse Lara <dmlaramartin@gmail.com>
2015-08-25 15:13:13 -0400

    Add layout style for _post partial

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,7 +3,7 @@ suckr = ImageSuckr::GoogleSuckr.new
 FactoryGirl.define do
 
   factory :post do
-    title Faker::Lorem.sentence(10).slice(0, 100)
+    title Faker::Lorem.sentence(7)
     description Faker::Lorem.paragraph(20)
     preview_image suckr.get_image_url({"q"=>"art"})
 
